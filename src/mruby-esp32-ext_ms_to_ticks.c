34d24f8709230de5fe80e406c67ed0a843a24afc
Seiei Miyagi <hanachin@gmail.com>
2018-04-07 14:45:03 +0900

    Fix gem init function name

--- src/mruby-esp32-ext_ms_to_ticks.c
+++ src/mruby-esp32-ext_ms_to_ticks.c
@@ -12,7 +12,7 @@ static mrb_value mrb_esp32_ms_to_ticks(mrb_state *mrb, mrb_value self) {
   return mrb_fixnum_value(ms / portTICK_RATE_MS);
 }
 
-void mrb_esp32_ext_esp_err_gem_init(mrb_state* mrb) {
+void mrb_esp32_ext_ms_to_ticks_gem_init(mrb_state* mrb) {
   struct RClass *mrb_esp32;
 
   /* ESP32 */
@@ -21,5 +21,5 @@ void mrb_esp32_ext_esp_err_gem_init(mrb_state* mrb) {
   mrb_define_singleton_method(mrb, mrb_esp32, "ms_to_ticks", mrb_esp32_ms_to_ticks, MRB_ARGS_REQ(1));
 }
 
-void mrb_esp32_ext_esp_err_gem_final(mrb_state* mrb) {
+void mrb_esp32_ext_ms_to_ticks_gem_final(mrb_state* mrb) {
 }
